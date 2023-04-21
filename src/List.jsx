c1aedc1e94ba959310798d67aae1a03965ec114c
Atharva123987 <atharva.amberkar@gmail.com>
2023-04-22 00:28:16 +0530

    Update List.jsx

--- src/List.jsx
+++ src/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from 'react';
+import React, { useState, useEffect, memo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 const WrappedSingleListItem = ({
@@ -26,18 +26,18 @@ WrappedSingleListItem.propTypes = {
 
 const SingleListItem = memo(WrappedSingleListItem);
 
-const WrappedListComponent = ({
+const WrappedListComponent = memo(({
   items, // items are passed from the App.js component
-}) => {
+})=> {
   const [selectedIndex, setSelectedIndex] = useState(null); // Issue 3 : Syntax error - useState returns the state variable and then the setter function
 
   useEffect(() => {
     setSelectedIndex(null);
   }, [items]);
 
-  const handleClick = index => {
+  const handleClick = useCallback(index => { // Optimization 1 : Using useCallback hook to memoize the handleClick function
     setSelectedIndex(index);
-  };
+  }, []);
 
   return (
     <ul style={{ textAlign: 'left' }}>
@@ -47,15 +47,15 @@ const WrappedListComponent = ({
           text={item.text}
           index={index}
           isSelected={selectedIndex === index} // Issue 4 : Passing boolean value to isSelected as its type is boolean
-          key={index} // Optimization 1 : Passing a key for letting react know which items have changed for optimization
+          key={index} // Optimization 2 : Passing a key for letting react know which items have changed for optimization
         />
       ))}
     </ul>
   )
-};
+});
 
 WrappedListComponent.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.shape({ // Issue 6 : Syntax error - Should be arrayOf and shape instead of array and shapeOf
+  items: PropTypes.arrayOf(PropTypes.shape({ // Issue 5 : Syntax error - Should be arrayOf and shape instead of array and shapeOf
     text: PropTypes.string.isRequired,
   })),
 };
